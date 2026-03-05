82842a49765d39d4c75f14979eef0febbd2090e1
Baileden <baileden@students.zhaw.ch>
2026-03-05 11:00:41 +0100

    Berechnungen ergänzt

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ st.set_page_config(page_title="Meine App", page_icon=":material/home:")
 
 pg_home = st.Page("views/home.py", title="Home", icon=":material/home:", default=True)
 pg_second = st.Page("views/unterseite_a.py", title="Unterseite A", icon=":material/info:")
+pg_third = st.Page("views/Berechnungen.py", title="Berechnungen", icon=":material/info:")
 
-pg = st.navigation([pg_home, pg_second])
+pg = st.navigation([pg_home, pg_second, pg_third])
 pg.run()
---
+++ views/Berechnungen.py
@@ -0,0 +1,37 @@
+import streamlit as st
+
+from functions.rechnungen import add, subtract, square, root
+
+st.title("Hier könnt ihr diverse Berechnungen durchführen")
+st.write(" Keine scheu, probiert es aus. ;) ")023
+
+with st.form("Addieren"):
+    st.write("Gib hier deine Zahlen die du addieren willst ein:")
+    a = st.number_input("Nummer 1")
+    b = st.number_input("Nummer 2")
+    submitted = st.form_submit_button("Berechnen")  
+    if submitted:
+        st.write("Ergebnis:", add(a, b))
+
+with st.form("Subtrahieren"):
+    st.write("Gib hier deine Zahlen die du subtrahieren willst ein:")
+    a = st.number_input("Nummer 1", key="sub_a")
+    b = st.number_input("Nummer 2", key="sub_b")
+    submitted = st.form_submit_button("Berechnen", key="sub_submit")  
+    if submitted:
+        st.write("Ergebnis:", subtract(a, b))
+
+with st.form("Quadrieren"):
+    st.write("Gib hier deine Zahl die du quadrieren willst ein:")
+    a = st.number_input("Nummer", key="square_a")
+    submitted = st.form_submit_button("Berechnen", key="square_submit")  
+    if submitted:
+        st.write("Ergebnis:", square(a))
+
+with st.form("Wurzel"):
+    st.write("Gib hier deine Zahl und die Wurzel welche daraus gezogen werden soll, ein:")
+    a = st.number_input("Nummer", key="root_a")
+    b = st.number_input("Wurzelgrad", key="root_b")
+    submitted = st.form_submit_button("Berechnen", key="root_submit")  
+    if submitted:
+        st.write("Ergebnis:", root(a, b))
