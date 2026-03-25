a855d522fea8e083ce5d7ddcbf1400c2fe9dc768
Baileden <baileden@students.zhaw.ch>
2026-03-25 18:16:01 +0100

    visualisierungen

--- views/Berechnungen.py
+++ views/Berechnungen.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 from datetime import datetime
 import pytz
+import plotly.graph_objects as go
+import plotly.express as px
 from utils.data_manager import DataManager
 from functions.rechnungen import add, subtract, square, root
 
@@ -69,3 +71,46 @@ with st.form("Wurzelziehen"):
 data_manager = DataManager()
 data_manager.save_user_data(st.session_state['data_df'], 'data.csv')
 st.dataframe(st.session_state['data_df'])
+
+# Visualisierung der Berechnungsresultate
+if len(st.session_state['data_df']) > 0:
+    st.subheader("📊 Visualisierung der Resultate")
+    
+    col1, col2 = st.columns(2)
+    
+    with col1:
+        # Balkendiagramm: Resultate pro Rechnung
+        fig1 = px.bar(
+            st.session_state['data_df'],
+            x='Rechnung',
+            y='Resultat',
+            title='Resultate nach Rechenoperation',
+            color='Resultat',
+            color_continuous_scale='Viridis'
+        )
+        fig1.update_layout(height=400)
+        st.plotly_chart(fig1, use_container_width=True)
+    
+    with col2:
+        # Pie-Diagramm: Anzahl Berechnungen pro Typ
+        rechnung_counts = st.session_state['data_df']['Rechnung'].apply(lambda x: x.split()[1] if len(x.split()) > 1 else x[0]).value_counts()
+        fig2 = go.Figure(data=[go.Pie(
+            labels=rechnung_counts.index,
+            values=rechnung_counts.values,
+            title='Verteilung der Rechenoperationen'
+        )])
+        fig2.update_layout(height=400)
+        st.plotly_chart(fig2, use_container_width=True)
+    
+    # Liniendiagramm: Resultate über Zeit
+    df_sorted = st.session_state['data_df'].sort_values('timestamp')
+    fig3 = px.line(
+        df_sorted,
+        x='timestamp',
+        y='Resultat',
+        color='Rechnung',
+        title='Resultate über die Zeit',
+        markers=True
+    )
+    fig3.update_layout(height=400, xaxis_title='Zeit', yaxis_title='Resultat')
+    st.plotly_chart(fig3, use_container_width=True)
